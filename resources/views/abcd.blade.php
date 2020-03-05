a1ea594aca233814faa54bdc0884ed2265e22991
nearoz998 <nearoz998@gmail.com>
2020-03-05 17:00:21 +0545

    commit

---
+++ resources/views/abcd.blade.php
@@ -0,0 +1,12 @@
+@extends('adminlte::page')
+
+@section('content')
+<a href='/' class='btn btn-primary'>Go back</a>
+    <div class='pl-3'>
+        <a href='/'>Home</a>
+        <h1>Testing page</h1>
+        <p>This one is the testing page</p>
+    </div>
+    <p class='text-end'><div class='text-center'><a href='/'>Back to home</div></p>
+    
+@endsection
